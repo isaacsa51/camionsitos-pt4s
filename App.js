b18b1829fd428434035be4fa3e89ee2f29890965
Isaac Serrano <serranoie99@gmail.com>
2020-12-13 12:13:57 -0700

    Prettier implementado

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation'
-import { createBottomTabNavigator } from 'react-navigation-tabs'
-import MapScreen from './src/screens/MapScreen'
-import MapListScreen from './src/screens/MapListScreen'
-import { setNavigator } from './src/NavigationRef'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
-const switchNavigator = createSwitchNavigator({
-  mainFlow: createBottomTabNavigator({
-    MapView: MapScreen,
-    MapList: MapListScreen
-  })
-})
-
-const App = createAppContainer(switchNavigator)
+const App = createAppContainer(switchNavigator);
 
 export default () => {
-  return (
-    <App
-      ref={navigator => {
-        setNavigator(navigator)
-      }}
-      />
-  );
-}
+	return (
+		<App
+			ref={(navigator) => {
+				setNavigator(navigator);
+			}}
+		/>
+	);
+};
